b3e6414688f00fcc6024b26e748b0726a8488efd
祝越 <zhuyue990727@qq.com>
2020-06-24 16:11:03 +0800

    添加项目文件。

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsControlLibrary1
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "输入文法";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 142);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "请输入文法";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(423, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "FIRSTVT与LASTVT集合";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "算符优先关系表";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(426, 120);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(359, 167);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 313);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(642, 276);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "执行文法分析";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(825, 623);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdg = new OpenFileDialog();
+            if (ofdg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = ofdg.FileName;//得到选择bai的文件的完整路径du                
+                textBox1.Text = System.IO.File.ReadAllText(file, Encoding.UTF8);//把读出来的数据显示在zhitextbox中            
+            }
+            }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+namespace GrammerAnalyzer
+{
+    class Grammer
+    {
+        StringCollection LeftPartRuleTemp = new StringCollection();
+        StringCollection RightPartRuleTemp = new StringCollection();
+        StringCollection LeftPartRule = new StringCollection();
+        StringCollection RightPartRule = new StringCollection();
+        ArrayList VT = new ArrayList();
+        ArrayList VN = new ArrayList();
+        bool[,] FirstVtTable;
+        bool[,] LastVtTable;
+        char[,] PreRelationTable;
+        StringCollection ProcessStr = new StringCollection();
+        Stack stack = new Stack();
+
+
+        /*构造函数，参数为规则数组，构造函数将规则分为两部分，分别存于LeftPartRuleTemp和RightPartRuleTemp中*/
+        public Grammer(String[] GrammerRules)
+        {
+            foreach (String ruleTemp in GrammerRules)
+            {
+                if (!ruleTemp.Contains("->")) return;
+                else
+                    ruleTemp.Replace("->", ">");
+                String[] temp = ruleTemp.Split('>');
+                LeftPartRule.Add(temp[0]);
+                RightPartRule.Add(temp[1]);
+            }
+        }
+
+        /*测试是否为算符优先文法，若是返回true，否返回false*/
+        private bool TestGrammer()
+        {
+            foreach (String tempRule in RightPartRuleTemp)
+            {
+                for (int i = 0; i <= tempRule.Length - 2; i++)
+                {
+                    if (tempRule[i] >= 'A' && tempRule[i] <= 'Z' && tempRule[i + 1] >= 'A' && tempRule[i + 1] <= 'Z') return false;
+                }
+
+            }
+            return true;
+        }
+
+        /*构造语法规则集合（去除|符号）*/
+        private void AddToGrammerRules(string LeftPart, string RightPart)
+        {
+            String partTemp = String.Empty;
+            for (int i = 0; i < RightPart.Length - 1; i++)
+            {
+                char vt = RightPart[i];
+                if (vt != '|')
+                    partTemp += vt.ToString();
+                else
+                {
+                    RightPartRule.Add(partTemp);
+                    LeftPartRule.Add(LeftPart[0].ToString());
+                    partTemp = String.Empty;
+                }
+                if (i == (RightPart.Length - 1))
+                {
+                    RightPartRule.Add(partTemp);
+                    LeftPartRule.Add(LeftPart[0].ToString());
+                    partTemp = String.Empty;
+                }
+            }
+        }
+
+
+        /*构造终结符集合和非终结符集合，输入的字符串应不包括|符号*/
+        private void AddToVtVn(string LeftPart, string RightPart)
+        {
+            if (!VN.Contains(LeftPart[0]))
+                VN.Add(LeftPart[0]);
+            foreach (char vt in RightPart)
+            {
+                if ((vt < 'A' && vt > 'Z') && !VT.Contains(vt) && vt != '|')
+                    VT.Add(vt);
+            }
+        }
+
+
+
+        /*返回对于输入文法的判断*/
+        public string RetJudgement()
+        {
+            string RetString = string.Empty;
+            if (TestGrammer())
+            {
+                RetString += "这是一个算符优先文法！ \r\n";
+                for (int i = 0; i <= LeftPartRuleTemp.Count - 1; i++)
+                {
+                    AddToGrammerRules(LeftPartRuleTemp[i].ToString(), RightPartRuleTemp[i].ToString());
+                    AddToVtVn(LeftPartRuleTemp[i].ToString(), RightPartRuleTemp[i].ToString());
+                }
+                RetString += "非终结符为： \r\n";
+                foreach (char vn in VN)
+                    RetString += vn.ToString();
+                RetString += "\r\n终结符为： \r\n";
+                foreach (char vt in VT)
+                    RetString += vt.ToString();
+            }
+            else RetString += "这不是一个算符优先文法";
+            return RetString;
+        }
+
+        private void Insert(char Vn, char Vt, bool[,] TempArr)
+        {
+            int VnNum = VN.IndexOf(Vn);
+            int VtNum = VT.IndexOf(Vt);
+            if (!TempArr[VnNum, VtNum])
+            {
+                TempArr[VnNum, VtNum] = true;
+                stack.Push(Vn.ToString() + Vt.ToString());
+            }
+        }
+
+
+        private string FirstVt() //计算FIRSTVT集合
+        {
+            string strFirst = "以下为FIRSTVT集合： \r\n";
+            stack.Clear();
+            FirstVtTable = new bool[VN.Count, VT.Count];
+            for (int i = 0; i <= LeftPartRule.Count - 1; i++)
+            {
+                if (VT.Contains(RightPartRule[i][0]))
+                {
+                    Insert(LeftPartRule[i][0], RightPartRule[i][0],
+                   FirstVtTable);
+                }
+                else if (RightPartRule[i].Length >= 2 &&
+                VT.Contains(RightPartRule[i][1]))
+                {
+                    Insert(LeftPartRule[i][0], RightPartRule[i][1],
+                   FirstVtTable);
+                }
+            }
+            while (stack.Count >= 1)
+            {
+                string tempQa = stack.Pop().ToString();
+                string tempQ = tempQa[0].ToString();
+                string tempa = tempQa[1].ToString();
+                for (int i = 0; i <= LeftPartRule.Count - 1; i++)
+                {
+                    if (RightPartRule[i][0].ToString() == tempQ)
+                    {
+                        Insert(LeftPartRule[i][0], tempa[0],
+                       FirstVtTable);
+                    }
+                }
+            }
+            for (int i = 0; i <= VN.Count - 1; i++)
+            {
+                strFirst += "FIRSTVT(" + VN[i].ToString() + ") ={ ";
+                for (int j = 0; j <= VT.Count - 1; j++)
+                {
+                    if (FirstVtTable[i, j] == true)
+                        strFirst += VT[j].ToString() + " ";
+                }
+                strFirst += "} \r\n";
+            }
+            return strFirst;
+        }
+        private string LastVt()
+        {
+            string strLast = "以下为LASTVT集合： \r\n";
+            stack.Clear();
+            LastVtTable = new bool[VN.Count, VT.Count];
+            for (int i = 0; i <= LeftPartRule.Count - 1; i++)
+            {
+                int j = RightPartRule[i].Length;
+                if (VT.Contains(RightPartRule[i][j - 1]))
+                {
+                    Insert(LeftPartRule[i][0], RightPartRule[i][j - 1],
+                  LastVtTable);
+                }
+                else if (RightPartRule[i].Length >= 2 &&
+                VT.Contains(RightPartRule[i][j - 2]))
+                {
+                    Insert(LeftPartRule[i][0], RightPartRule[i][j - 2],
+                  LastVtTable);
+                }
+            }
+            while (stack.Count >= 1)
+            {
+                string tempQa = stack.Pop().ToString();
+                string tempQ = tempQa[0].ToString();
+                string tempa = tempQa[1].ToString();
+                for (int i = 0; i <= LeftPartRule.Count - 1; i++)
+                {
+                    int k = RightPartRule[i].Length;
+                    if (RightPartRule[i][k - 1].ToString() == tempQ)
+                    {
+                        Insert(LeftPartRule[i][0], tempa[0], LastVtTable);
+                    }
+                }
+            }
+            for (int i = 0; i <= VN.Count - 1; i++)
+            {
+                strLast += "LASTVT(" + VN[i].ToString() + ") ={ ";
+                for (int j = 0; j <= VT.Count - 1; j++)
+                {
+                    if (LastVtTable[i, j] == true)
+                        strLast += VT[j].ToString() + " ";
+                }
+                strLast += "} \r\n";
+            }
+            return strLast;
+        } //计算LASTVT集合
+        public string RetFirstLastVt()
+        {
+            return FirstVt() + "\r\n\r\n" + LastVt();
+        }
+        private void RetPreRelationTable()
+        {
+            PreRelationTable = new char[VT.Count, VT.Count];
+            foreach (string tempRule in RightPartRule)
+            {
+                for (int i = 0; i <= tempRule.Length - 2; i++)
+                {
+                    if (VT.Contains(tempRule[i]) && VT.Contains(tempRule[i
+                   + 1]))
+                    {
+                        PreRelationTable[VT.IndexOf(tempRule[i]),
+                      VT.IndexOf(tempRule[i + 1])] = '=';
+                    }
+                    if (i <= tempRule.Length - 3 &&
+                    VT.Contains(tempRule[i]) && !VT.Contains(tempRule[i + 1]) &&
+                    VT.Contains((tempRule[i + 2])))
+                    {
+                        PreRelationTable[VT.IndexOf(tempRule[i]),
+                       VT.IndexOf(tempRule[i + 2])] = '=';
+                    }
+                    if (VT.Contains(tempRule[i]) && !VT.Contains(tempRule[i + 1]))
+                    {
+                        for (int j = 0; j <= VT.Count - 1; j++)
+                        {
+                            if (FirstVtTable[VN.IndexOf(tempRule[i + 1]),
+                          j] == true)
+                                PreRelationTable[VT.IndexOf(tempRule[i]), j] = '<';
+                        }
+                    }
+                    if (!VT.Contains(tempRule[i]) && VT.Contains(tempRule[i + 1]))
+                    {
+                        for (int j = 0; j <= VT.Count - 1; j++)
+                        {
+                            if (LastVtTable[VN.IndexOf(tempRule[i]), j] == true)
+                                PreRelationTable[j,
+                                VT.IndexOf(tempRule[i + 1])] = '>';
+                        }
+                    }
+                }
+            }
+        }
+        public StringCollection DrawPreRelationTable()
+        {
+            RetPreRelationTable();
+            StringCollection tableStr = new StringCollection();
+            string tempstring = " \t";
+            for (int i = 0; i <= VT.Count - 1; i++)
+            {
+                tempstring += VT[i].ToString() + "\t";
+            }
+            tableStr.Add(tempstring);
+            for (int i = 0; i <= VT.Count - 1; i++)
+            {
+                tempstring = string.Empty;
+                tempstring += VT[i].ToString() + "\t";
+                for (int j = 0; j <= VT.Count - 1; j++)
+                {
+                    tempstring += PreRelationTable[i, j].ToString() + "\t";
+                }
+                tableStr.Add(tempstring);
+            }
+            return tableStr;
+        }
+        private bool AnalysisProcess(string InputSentence) 
+        {
+            return true;
+        }
+private string TraceBack(string s, int m, int n)
+        {
+            return s;
+        }
+ 
+public StringCollection RetAnalysisProcess(string InputSentence)
+        {
+            ProcessStr.Clear();
+            if (AnalysisProcess(InputSentence))
+                return ProcessStr;
+            else
+            {
+                ProcessStr.Add("error");
+                MessageBox.Show("在输入的句子中发现语法错误！","提示！");
+            return ProcessStr;
+            }
+        }
+    }
+}
